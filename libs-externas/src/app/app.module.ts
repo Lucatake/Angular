a8f52a48fbfe3fde608e69b23e0de3337cb8807b
Luana <1295943@sga.pucmias.br>
2022-01-19 15:17:11 -0300

    libs externas

---
+++ libs-externas/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { MaterializeModule } from 'angular2-materialize';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
