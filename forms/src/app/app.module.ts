2caf36ae8ed229b6123f2b489808d8850573bfa7
Luana <1295943@sga.pucmias.br>
2022-01-25 11:40:30 -0300

    forms aula 76 formsModule

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
+import { TemplateFormModule } from './template-form/template-form.module';
+//import { TemplateFormComponent } from './template-form/template-form.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent,
-    HomeComponent
+    HomeComponent,
+    //TemplateFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
