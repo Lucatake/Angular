6624add3585c489c3597c0d84ed06377aa3f72fc
Luana Takeishi <luana.takeishi@corp.sigga.com>
2022-02-02 22:49:52 -0300

    busca reativa (prog reativa e funcional)

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, Observable, tap } from 'rxjs';
+import { filter, map, Observable, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -13,12 +14,27 @@ export class LibSearchComponent implements OnInit {
   query = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   //Mostra campos buscado por Angular para testes -> this.SEARCH_URL+this.fields
-  //readonly fields: string = '?fields=name,description,version,homepage&search=angular';
+  readonly FIELDS: string =
+    '?fields=name,description,version,homepage&search=angular';
   results$: Observable<any> = new Observable();
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.results$ = this.query.valueChanges.pipe(
+      map((value) => value.trim()),
+      filter((value) => value.length > 2),
+      debounceTime(100),
+      distinctUntilChanged(),
+      switchMap((value) =>
+        this.http.get(this.SEARCH_URL, {
+          params: { search: value, fields: this.FIELDS },
+        })
+      ),
+      tap((res: any) => (this.total = res.total)),
+      map((res: any) => res.results)
+    );
+  }
 
   onSearch() {
     const fields = 'name,description,version,homepage';
@@ -26,23 +42,22 @@ export class LibSearchComponent implements OnInit {
 
     const params_ = {
       search: value,
-      fields: fields
+      fields: fields,
     };
 
     let params = new HttpParams();
     params = params.set('search', value);
     params = params.set('fields', fields);
 
-
     if (value && (value = value.trim()) !== '') {
       this.results$ = this.http
-        //.get(`${this.SEARCH_URL}?fields=${fields}&search=${value}`)
+        //.get(`${this.SEARCH_URL}?fields=${FIELDS}&search=${value}`)
         //monta a url
-        .get(this.SEARCH_URL, {params})
+        .get(this.SEARCH_URL, { params })
         .pipe(
-        tap((res: any) => (this.total = res.total)),
-        map((res: any) => res.results)
-      );
+          tap((res: any) => (this.total = res.total)),
+          map((res: any) => res.results)
+        );
     }
   }
 }
