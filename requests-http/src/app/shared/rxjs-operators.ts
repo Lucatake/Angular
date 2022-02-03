6603978a0ddc224ce49a18010499ebcedbf4a1c0
Luana Takeishi <luana.takeishi@corp.sigga.com>
2022-02-03 17:42:03 -0300

    alteração rxjs

--- requests-http/src/app/shared/rxjs-operators.ts
+++ requests-http/src/app/shared/rxjs-operators.ts
@@ -4,8 +4,9 @@ import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 
 export function filterResponse<T>() {
   return pipe(
-    //filter((event: HttpEvent<T>) => event.type === HttpEventType.Response),
-    map((res: HttpResponse<T>) => res.body)
+    filter((event: HttpEvent<T>) => event.type === HttpEventType.Response),
+    //map((res: HttpResponse<T>) => res.body)
+    map((res: HttpEvent<T>) => res)
   );
 }
 
